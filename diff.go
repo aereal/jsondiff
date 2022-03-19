f1166d4f6aec006143588197837d62d0cee55fb5
aereal <aereal@aereal.org>
2022-03-20 00:16:48 +0900

    feat: add Diff()

--- diff.go
+++ diff.go
@@ -1,8 +1,91 @@
 package jsondiff
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+
+	"github.com/hexops/gotextdiff"
+	"github.com/hexops/gotextdiff/myers"
+	"github.com/hexops/gotextdiff/span"
 	"github.com/itchyny/gojq"
 )
+
+type opt struct {
+	ignore *gojq.Query
+}
+
+// Option is a function to modify Diff's behavior.
+type Option func(*opt)
+
+// Ignore returns Option function that indicates the function to ignore structures pointed by the query.
+func Ignore(query *gojq.Query) Option {
+	return func(o *opt) {
+		o.ignore = query
+	}
+}
+
+// Diff calculates differences with lhs and rhs.
+func Diff(lhs, rhs interface{}, opts ...Option) (string, error) {
+	o := &opt{}
+	for _, f := range opts {
+		f(o)
+	}
+	var (
+		left  = lhs
+		right = rhs
+	)
+	if o.ignore != nil {
+		var err error
+		q := withUpdate(o.ignore)
+		left, err = modifyValue(q, lhs)
+		if err != nil {
+			return "", fmt.Errorf("modify(lhs): %v", err)
+		}
+		right, err = modifyValue(q, rhs)
+		if err != nil {
+			return "", fmt.Errorf("modify(rhs): %v", err)
+		}
+	}
+	l, err := toJSON(left)
+	if err != nil {
+		return "", fmt.Errorf("toJSON(lhs): %v", err)
+	}
+	r, err := toJSON(right)
+	if err != nil {
+		return "", fmt.Errorf("toJSON(rhs): %v", err)
+	}
+	edits := myers.ComputeEdits(span.URIFromPath(""), l, r)
+	d := gotextdiff.ToUnified("lhs", "rhs", l, edits)
+	return fmt.Sprint(d), nil
+}
+
+func modifyValue(query *gojq.Query, x interface{}) (interface{}, error) {
+	iter := query.Run(x)
+	var ret interface{}
+	for {
+		got, hasNext := iter.Next()
+		if !hasNext {
+			break
+		}
+		if err, ok := got.(error); ok {
+			return nil, err
+		}
+		ret = got
+	}
+	return ret, nil
+}
+
+func toJSON(x interface{}) (string, error) {
+	b := new(bytes.Buffer)
+	enc := json.NewEncoder(b)
+	enc.SetIndent("", "  ")
+	if err := enc.Encode(x); err != nil {
+		return "", err
+	}
+	b.WriteRune('\n')
+	return b.String(), nil
+}
 
 func withUpdate(query *gojq.Query) *gojq.Query {
 	var ret *gojq.Query
--- diff_test.go
+++ diff_test.go
@@ -7,6 +7,21 @@ import (
 	"github.com/itchyny/gojq"
 )
 
+func TestDiff(t *testing.T) {
+	q := parseQuery(t, ".b, .c")
+	lhs := map[string]interface{}{"a": 1, "b": 2, "c": 3, "d": 4}
+	rhs := map[string]interface{}{"a": 1, "b": 1, "c": 2, "d": 3}
+	got, err := Diff(lhs, rhs, Ignore(q))
+	if err != nil {
+		t.Fatal(err)
+	}
+	var want string = "--- lhs\n+++ rhs\n@@ -2,6 +2,6 @@\n   \"a\": 1,\n   \"b\": null,\n   \"c\": null,\n-  \"d\": 4\n+  \"d\": 3\n }\n \n"
+	if got != want {
+		t.Log(got)
+		t.Errorf("got:\n%q\nwant:\n%q", got, want)
+	}
+}
+
 func Test_withUpdate(t *testing.T) {
 	queries := []struct {
 		query string
